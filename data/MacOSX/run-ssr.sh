834c36528c5704fec42aa46a444a60c58a434f43
Matthias Geier <Matthias.Geier@gmail.com>
2012-12-03 13:16:37 +0100

    Fix run-ssr.sh
    * use bash instead of sh
    * add ""
    * correct use of $OPTIONS array

--- data/MacOSX/run-ssr.sh
+++ data/MacOSX/run-ssr.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-thispath=$(dirname "$0")
+thispath="$(dirname "$0")"
 
 # add all except -psn* to $OPTIONS:
 while [[ $1 ]]; do
@@ -18,4 +18,4 @@ done
 # Run script with absolute path to SSR binary as parameter.
 # Script runs in background process (&), else we would have two SSR icons in the dock.
 # For relative paths to work, this script and the SSR binary must be placed in MacOS/ folder of .app bundle.
-osascript "$thispath/run-ssr.scpt" "$thispath" $OPTIONS &
+osascript "$thispath/run-ssr.scpt" "$thispath" "${OPTIONS[@]}" &
